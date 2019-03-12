a3e6cf5501b912bb4428d4da7286573cf30a6dd3
Suparna <t0915635@u.nus.edu>
2019-03-12 14:35:15 +0800

    working mimic baseline data prep

--- baseline_prep.py
+++ baseline_prep.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 import numpy as np
 from math import ceil, isnan, floor
 #script to generate baseline demographics of patient of MIMIC3 - 9 demographics
@@ -13,21 +14,55 @@ def baselines():
     lines = [x for x in lines if x[1]!='absent']
     subject_ids = [int(x[0]) for x in lines]
     cols = ["Gender", "Age", "Ethnicity", "Religion", "Language", "Height", "Weight", "Marital_Status", "Insurance"]
+    gender_map = {}
+    ethnicity_map = {}
+    religion_map = {}
+    language_map = {}
+    marital_map = {}
+    insurance_map = {}
     for sub in subject_ids:
+        print("Processing subject:%s"%(str(sub)))
         person = admissions[admissions.SUBJECT_ID==sub]
-        insurance = person.INSURANCE[0]
-        language = person.LANGUAGE[0]
-        religion = person.RELIGION[0]
-        marital = person.MARITAL_STATUS[0]
-        ethnicity = person.ETHNICITY[0]
+        #print person
+        insurance = person.INSURANCE.iloc[0]
+        if insurance not in insurance_map.keys():
+            insurance_map[insurance] = len(insurance_map)
+        insurance = insurance_map[insurance]
+        #print insurance_map
+        language = person.LANGUAGE.iloc[0]
+        if language not in language_map.keys():
+            language_map[language] = len(language_map)
+        language = language_map[language]
+        religion = person.RELIGION.iloc[0]
+        if religion not in religion_map.keys():
+            religion_map[religion] = len(religion_map)
+        religion = religion_map[religion]
+        marital = person.MARITAL_STATUS.iloc[0]
+        if marital not in marital_map.keys():
+            marital_map[marital] = len(marital_map)
+        marital = marital_map[marital]
+        ethnicity = person.ETHNICITY.iloc[0]
+        if ethnicity not in ethnicity_map.keys():
+            ethnicity_map[ethnicity] = len(ethnicity_map)
+        ethnicity = ethnicity_map[ethnicity]
         stays = pd.read_csv(dest_path+str(sub)+'/stays.csv')
         for stay_no in range(stays.shape[0]):
-            epi = pd.read_csv(dest_path+str(sub)+'/episode'+stay_no+'.csv')
-            gender = epi.GENDER[0]
-            age = epi.AGE[0]
-            height = epi.HEIGHT[0]
-            weight = epi.WEIGHT[0]
+            try:
+                epi = pd.read_csv(dest_path+str(sub)+'/episode'+str(stay_no+1)+'.csv')
+            except:
+                continue
+            gender = epi.Gender.iloc[0]
+            if gender not in gender_map.keys():
+                gender_map[gender] = len(gender_map)
+            gender = gender_map[gender]
+            age = epi.Age.iloc[0]
+            height = epi.Height.iloc[0]
+            weight = epi.Weight.iloc[0]
             data = [gender, age, ethnicity, religion, language, height, weight, marital, insurance]
-            pd.DataFrame(data=[data], columns=cols).to_csv(dest_path+str(sub)+'/baseline'+str(stay_no)+'.csv')
-            
-    
+            #print data
+            df = pd.DataFrame(data=[data], columns=cols)
+            #print df
+            df.to_csv(dest_path+str(sub)+'/baseline'+str(stay_no)+'.csv')
+
+if __name__=="__main__":
+    baselines()
--- patient_events.py
+++ patient_events.py
@@ -36,27 +36,33 @@ def prep_baseline_mgp():
     eth_count = 0
     Gender = {'M':0, 'F':1}
 
-    for sub in enumerate(subject_ids):
+    for sub in subject_ids:
         Y_i = []
         ind_kf_i = []
         ind_kt_i = []
-        stays = pd.read_csv(data_path+'/root/'+str(sub)+'/stays.csv', parse_dates=True)
+        stays = pd.read_csv(data_path+'root/'+str(sub)+'/stays.csv', parse_dates=True)
         #timeline = pd.read_csv('~/mimic3-benchmarks/data/root/'+str(subject_indices[index])+'/'+str(sub)+'/episode1_timeseries.csv')
         intime = pd.to_datetime(stays['INTIME'])
         label = stays['MORTALITY_INHOSPITAL'][0]
         for stay_no in range(stays.shape[0]):
-            timeline = pd.read_csv(data_path+'/root/'+str(sub)+'/episode'+str(stay_no)+'.csv')
+            try:
+                timeline = pd.read_csv(data_path+'root/'+str(sub)+'/episode'+str(stay_no+1)+'_timeseries.csv')
+            except:
+                continue
             timeline = timeline[timeline.Hours>=0]
             T_i = timeline.Hours
             column_map = {n:i for i,n in enumerate(list(timeline.columns))}
             len_columns = len(timeline.columns)
             for t in range(T_i.shape[0]):
                 values = timeline.iloc[t]
+                #print values
+                #values = np.log(values)
+                #print values
                 presence = timeline.iloc[t].isnull()
                 for i in range(1,len_columns):
                     if presence[i]==False:
                         if type(values[i]) is not str:
-                            Y_i.append(values[i])
+                            Y_i.append(np.log(values[i]))
                             ind_kf_i.append(i-1)
                             ind_kt_i.append(t)
             end_times.append(timeline.iloc[-1][0])
@@ -65,29 +71,35 @@ def prep_baseline_mgp():
             num_obs_values.append(len(Y_i))
             num_rnn_grid_times.append(ceil(end_times[-1])+1)
             rnn_grid_times.append(list(np.arange(num_rnn_grid_times[-1])))
-            baseline = pd.read_csv(data_path+'/root/'+str(sub)+'/baseline'+str(stay_no)+'.csv')
-            baseline_i = baseline.iloc[0]
-            medicines = pd.read_csv(data_path+'/medicines/'+str(sub)+'_stay'+str(stay_no)+'.med')
+            baseline = pd.read_csv(data_path+'root/'+str(sub)+'/baseline'+str(stay_no)+'.csv', )
+            baseline_i = baseline.iloc[0].to_list()
+            #print baseline_i
+            medicines = pd.read_csv(data_path+'medicines/'+str(sub)+'_stay'+str(stay_no)+'.med')
             meds_on_grid_i = medicines.to_numpy()
-            meds_on_grid.append(meds_on_grid_i)
+            meds_on_grid.append(meds_on_grid_i.tolist())
             baseline_covs.append(baseline_i)
             Y.append(Y_i)
             ind_kf.append(ind_kf_i)
             ind_kt.append(ind_kt_i)
             T.append(T_i.tolist())
             labels.append(label)
-    print num_obs_times
+    print("num of observation times: %s"%num_obs_times)
     print num_obs_values
     print num_rnn_grid_times
     print rnn_grid_times
     print labels
     print T
+    print "printing Y"
     print Y
+    print "kf"
     print ind_kf
+    print "kt"
     print ind_kt
+    print "meds"
     print meds_on_grid
+    print "baselines"
     print baseline_covs
     return (num_obs_times,num_obs_values,num_rnn_grid_times,rnn_grid_times,labels,T,Y,ind_kf,ind_kt,meds_on_grid,baseline_covs)
 
 if __name__=="__main__":
-    dataset_prep()
+    prep_baseline_mgp()
